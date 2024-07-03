718d2fa0ff05b998d3746dde3e036ad482213ddf
lepikova <lepikova.anna@yandex.ru>
2024-07-04 00:54:57 +0300

    тест создание контакта

---
+++ contact.py
@@ -0,0 +1,18 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, address, company, homenomber,
+                         worknomber, mobilenomber, fax, email, bday, bmonth, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.address = address
+        self.company = company
+        self.homenomber = homenomber
+        self.worknomber = worknomber
+        self.mobilenomber = mobilenomber
+        self.fax = fax
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
