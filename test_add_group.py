72c7a26a8afb5243f2ca083e5862f9e2ffc6e478
lepikova <lepikova.anna@yandex.ru>
2024-07-15 08:39:26 +0300

    случайно удалила тесты по группе (вернула обратно)

---
+++ test_add_group.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from group import Group
+from application import Application
+import pytest
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group(name="rewq", header="anny", footer="cloud"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
