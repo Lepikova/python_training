dc318ff8b7fa458e0d3ee4cb4a0cddc1c3b4f7cf
lepikova <lepikova.anna@yandex.ru>
2024-07-15 11:05:01 +0300

    добавлен переход на страницу контактов обновление всех полей, заполненных ранее, в контактах

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,6 +7,7 @@ class ContactHelper:
 
     def creating(self, contact):
         wd = self.app.wd
+        self.open_contact_page()
         self.open_creating_contact()
         # fill contact form
         wd.find_element_by_name("firstname").click()
@@ -63,6 +64,7 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
+        self.open_contact_page()
         # select first contact
         wd.find_element_by_name("selected[]").click()
         # submit deletion
@@ -70,12 +72,58 @@ class ContactHelper:
         self.app.return_to_home_page()
 
     def update_contact(self, contact):
+        self.open_contact_page()
         wd = self.app.wd
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homenomber)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.worknomber)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobilenomber)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.app.return_to_home_page()
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        # open contact page
+        wd.find_element_by_link_text("home").click()
--- test/test_change_contact.py
+++ test/test_change_contact.py
@@ -3,5 +3,5 @@ from model.contact import Contact
 
 def test_update_contact(app):
     app.session.login("admin", "secret")
-    app.contact.update_contact(Contact(firstname="Igor", middlename="Iva", lastname="Ivanov", nickname="Vano", title="First", address="Spb", company="Leno", homenomber="123", worknomber="12344", mobilenomber="32123", fax="321", email="1234@mail.ru", bday="20", bmonth="October", byear="1990"))
+    app.contact.update_contact(Contact(firstname="Igor", middlename="Semen", lastname="Petrov", nickname="wqer", title="Second", address="Moscow", company="Magnit", homenomber="proton", worknomber="00000", mobilenomber="54345", fax="77777", email="1234@yandex.ru", bday="10", bmonth="November", byear="1987"))
     app.session.logout()
