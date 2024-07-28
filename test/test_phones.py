8fd1790d5c9178eb34ce37a28507071519ee5bc3
lepikova <lepikova.anna@yandex.ru>
2024-07-28 19:24:31 +0300

    Реализованы проверки для всех полей контакта на главной странице (удалила ненужный print)

--- test/test_phones.py
+++ test/test_phones.py
@@ -11,17 +11,7 @@ def tests_phones_on_home_page(app):
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    print(contact_from_home_page.all_phones_from_home_page)
-    print(contact_from_home_page.lastname)
-    print(contact_from_home_page.firstname)
-    print(contact_from_home_page.address)
-    print(contact_from_home_page.all_emails_from_home_page)
-    print("nexxxt")
-    print(merge_phones_like_on_home_page(contact_from_edit_page))
-    print(contact_from_edit_page.lastname)
-    print(contact_from_edit_page.firstname)
-    print(contact_from_edit_page.address)
-    print(merge_emails_like_on_home_page(contact_from_edit_page))
+
 
 #def test_phones_on_contact_view_page(app):
 #    contact_from_view_page = app.contact.get_contact_from_view_page(0)
