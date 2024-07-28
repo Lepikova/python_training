74119d06da5f58f8b8237811e78111bb05443124
lepikova <lepikova.anna@yandex.ru>
2024-07-29 00:52:34 +0300

    Параметризованы тесты для создания групп и контактов

--- fixture/group.py
+++ fixture/group.py
@@ -9,12 +9,12 @@ class GroupHelper:
         # return to groups page
         wd.find_element_by_link_text("group page").click()
 
-    def create(self, party):
+    def create(self, group):
         wd = self.app.wd
         self.open_group_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        self.fill_group_form(party)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -40,7 +40,7 @@ class GroupHelper:
             wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
-      self.delete_group_by_index(0)
+        self.delete_group_by_index(0)
 
     def delete_group_by_index(self, index):
         wd = self.app.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,48 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*4
+    return prefix + "".join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
+
+def random_phone_number():
+    digits = string.digits
+    return "7" + "".join([random.choice(digits) for _ in range(10)])
+
+def random_month():
+    months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ]
+    return random.choice(months)
+
+def random_day():
+    return str(random.randint(1, 31))
+
+def random_year():
+    return str(random.randint(1900, 2024))
+
+def random_email(prefix, maxlen):
+    domains = ["gmail.com", "yahoo.com", "mail.ru", "yandex.ru", "outlook.com"]
+    return (prefix + "".join([random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(1, maxlen))])
+            + "@" + random.choice(domains))
+
+testdata = [Contact(firstname=random_string("first", 10), middlename=random_string("middle", 10),
+                    lastname=random_string("last", 10), nickname=random_string("nick", 10),
+                    title=random_string("title", 10), address=random_string("address", 30),
+                    company=random_string("comp", 10), homenomber=random_phone_number(),
+                    worknomber=random_phone_number(), mobilenomber=random_phone_number(), fax=random_phone_number(),
+                    email=random_email("email", 10), email2=random_email("email", 10),
+                    email3=random_email("email", 10), bday=random_day(), bmonth=random_month(), byear=random_year())
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Iva", lastname="Ivanov", nickname="Vano",
-                      title="First", address="Санкт-Петербург, Ленинский, 22-4", company="Leno", homenomber="123", worknomber="12344",
-                      mobilenomber="32123", fax="321", email="1234@mail.ru", email2="1234@ya.ru",
-                      email3="1234@ou.ru", bday="20", bmonth="October", byear="1990")
     app.contact.creating(contact)
     assert len(old_contacts) + 1 == app.contact.count_contacts()
     new_contacts = app.contact.get_contact_list()
