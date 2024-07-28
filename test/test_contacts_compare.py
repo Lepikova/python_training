8b919e779fffb8b57dc94ad740d459e7bcc4990a
lepikova <lepikova.anna@yandex.ru>
2024-07-28 19:28:13 +0300

    Реализованы проверки для всех полей контакта на главной странице (изменила название)

--- test/test_contacts_compare.py
+++ test/test_contacts_compare.py
@@ -1,7 +1,7 @@
 import re
 from random import randrange
 
-def tests_phones_on_home_page(app):
+def tests_contactes_on_home_page(app):
     contact_list = app.contact.get_contact_list()
     index = randrange(len(contact_list))
     contact_from_home_page = app.contact.get_contact_list()[index]
